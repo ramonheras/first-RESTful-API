45b57a91c8c8466e78de391188c0d03891f1929b
Ramon <ramon98heras@gmail.com>
2021-08-27 20:02:15 +0200

    Added eslint and airbnb-style

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import express from 'express'
+import express from 'express';
 
 const app = express();
 
 app.get('/', (req, res) => res.send('Hello World'));
 
-app.listen(3000, () => console.log('Listening on port 3000')); 
+app.listen(3000, () => console.log('Listening on port 3000'));
